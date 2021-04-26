335ec66b7a57cfa233caef5c0022687df56e2965
nootisme <80405724+nootisme@users.noreply.github.com>
2021-04-26 11:05:48 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os # needed for env
+from dotenv import load_dotenv # env
+
+load_dotenv("token.env")
+token = os.getenv('token')
+
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print("Nooty Noot is locked and loaded.")
+
+    bot.load_extension('cogs.reddit')
+
+    bot.run()
+
+bot.run(token)
